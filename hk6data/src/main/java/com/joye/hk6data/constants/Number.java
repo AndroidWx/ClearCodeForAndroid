606ab8c6b1aaa590df3c752729df503a0ff1b757
AndroidWx <wxdreamfly@gmail.com>
2016-09-23 00:15:34 +0800

    新增Number

---
+++ hk6data/src/main/java/com/joye/hk6data/constants/Number.java
@@ -0,0 +1,108 @@
+package com.joye.hk6data.constants;
+
+/**
+ * Created by xiang on 16/9/22.
+ */
+
+public class Number {
+    //http://www.cnblogs.com/jara/p/4122377.html
+    public static final int N_1=1;
+
+    public static final int N_2=2;
+
+    public static final int N_3=3;
+
+    public static final int N_4=4;
+
+    public static final int N_5=5;
+
+    public static final int N_6=6;
+
+    public static final int N_7=7;
+
+    public static final int N_8=8;
+
+    public static final int N_9=9;
+
+    public static final int N_10=10;
+
+    public static final int N_11=11;
+
+    public static final int N_12=12;
+
+    public static final int N_13=13;
+
+    public static final int N_14=14;
+
+    public static final int N_15=15;
+
+    public static final int N_16=16;
+
+    public static final int N_17=17;
+
+    public static final int N_18=18;
+
+    public static final int N_19=19;
+
+    public static final int N_20=20;
+
+    public static final int N_21=21;
+
+    public static final int N_22=22;
+
+    public static final int N_23=23;
+
+    public static final int N_24=24;
+
+    public static final int N_25=25;
+
+    public static final int N_26=26;
+
+    public static final int N_27=27;
+
+    public static final int N_28=28;
+
+    public static final int N_29=29;
+
+    public static final int N_30=30;
+
+    public static final int N_31=31;
+
+    public static final int N_32=32;
+
+    public static final int N_33=33;
+
+    public static final int N_34=34;
+
+    public static final int N_35=35;
+
+    public static final int N_36=36;
+
+    public static final int N_37=37;
+
+    public static final int N_38=38;
+
+    public static final int N_39=39;
+
+    public static final int N_40=40;
+
+    public static final int N_41=41;
+
+    public static final int N_42=42;
+
+    public static final int N_43=43;
+
+    public static final int N_44=44;
+
+    public static final int N_45=45;
+
+    public static final int N_46=46;
+
+    public static final int N_47=47;
+
+    public static final int N_48=48;
+
+    public static final int N_49=49;
+
+
+}
